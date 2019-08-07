338db4c9233ad99dc08e99c820da24c945b2a80b
ALuis26 <annaluis26@gmail.com>
2019-08-07 15:42:51 +0200

    back end first aproach

---
+++ routes/fakeusers.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    id: "1",
+    userName: "Ainura",
+    userPassword: "ainura",
+    userEmail: "ainura@gmail.com"
+  },
+
+  {
+    id: "2",
+    userName: "Emefa",
+    userPassword: "emefa",
+    userEmail: "emefa@gmail.com"
+  },
+
+  {
+    id: "3",
+    userName: "Alejandra",
+    userPassword: "alejandra",
+    userEmail: "alejandra@gmail.com"
+  },
+
+  {
+    id: "4",
+    userName: "Anna",
+    userPassword: "anna",
+    userEmail: "anna@gmail.com"
+  }
+];
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,65 @@
 var express = require("express");
 var router = express.Router();
+var jwt = require("jsonwebtoken");
+var fakeUsers = require("./fakeusers.js");
+const db = require("./../model/helper");
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.json("This is the API home route. If you read this means the API connection is working!");
+  // res.json(
+  //   "This is the API home route. If you read this means the API connection is working!"
+  // );
+  res.send(fakeUsers);
 });
+
+var superSecret = "costabrava";
+// route for authenticating users:
+router.post("/login", function(req, res, next) {
+  var name = req.body.userName;
+  var password = req.body.userPassword;
+  // find the user
+
+  var user = fakeUsers.filter(function(e) {
+    e.userName == name;
+  });
+  res.send(user);
+  if (!user) {
+    res.status(404).send("User does not exist");
+  } else {
+    // check if password matches
+    if (user[0].userPassword !== password) {
+      res.status(404).send("Wrong password");
+    } else {
+      //if password is right and user is right, generate a token
+      var token = jwt.sign(
+        {
+          userId: user[0].id
+        },
+        superSecret,
+        {
+          expiresInMinutes: 1440 // expires in 24 hours
+        }
+      );
+      // return the information including token as JSON
+      res.json({
+        token: token
+      });
+    }
+  }
+});
+
+// if user is found and password is right
+// create a token
+
+// return the information including token as JSON
+
+//validate if the credentials are ok
+//if they are ok, check if a token exists
+//if it does not exists, generate a token for this user
+//return the token to the user
+//   res.json({ accessToken: "faketoken" });
+//  });
+
+router.use(function(req, res, next) {});
 
 module.exports = router;

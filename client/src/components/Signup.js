79928ea9a4097472f593aa2c66ef9fd0938dc581
Emefa Senoo <emefa.senoo@gmail.com>
2019-08-08 16:30:03 +0200

    login token authentication

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,11 +1,52 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+// import Alert from "react-bootstrap/Alert";
+import Col from "react-bootstrap/Col";
 
 export default class Signup extends Component {
-    render() {
-        return (
-            <div>
-                Sign Up Page
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Form className="signup-form">
+          <h1>Sign Up Here!</h1>
+          <br />
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridUsername">
+              <Form.Label>User Name</Form.Label>
+              <Form.Control type="username" placeholder="Select a username" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGriduserPassword">
+              <Form.Label>userPassword</Form.Label>
+              <Form.Control type="password" placeholder="password" />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridConfirmuserPassword">
+              <Form.Label>Confirm userPassword</Form.Label>
+              <Form.Control type="password" placeholder="Confirm password" />
+            </Form.Group>
+          </Form.Row>
+
+          <Form.Group id="formGridCheckbox">
+            <Form.Check
+              type="checkbox"
+              label="I accept the terms and conditons"
+            />
+          </Form.Group>
+
+          <Button variant="primary" type="submit">
+            Sign Up!
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }

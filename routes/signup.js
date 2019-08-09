2873b772665e5c99c4754e7bbb75a00a0e40cf1b
ALuis26 <annaluis26@gmail.com>
2019-08-09 17:36:27 +0200

    add sign up backend

--- routes/signup.js
+++ routes/signup.js
@@ -4,20 +4,27 @@ var db = require("../model/helper");
 
 /* GET home page. */
 router.post("/", function(req, res, next) {
-  var name = req.header.userName;
-  var password = req.header.userPassword;
-  var email = req.header.userEmail;
-  var id = req.header.id;
+  var userName = req.headers.name;
+  var password = req.headers.password;
+  var email = req.headers.email;
+  var id = req.headers.id;
 
   // SELECT * FROM users WHERE userName="Ainura";
 
+  // res.send({ userName });
+
   db(
-    "INSERT INTO users(id, userPassword, userEmail, userName) VALUES ('"+id+"",
-    "+password+",
-    name+,
-    email + ');"
+    "INSERT INTO users(id, userPassword, userEmail, userName) VALUES ('" +
+      id +
+      "', '" +
+      password +
+      "', '" +
+      userName +
+      "', '" +
+      email +
+      "');"
   ).then(results => {
-    res.send(results);
+    res.status(200).send("user created");
   });
 });
 

36bb1ef2249c921c6db095e7852b743d65d042e1
ALuis26 <annaluis26@gmail.com>
2019-08-13 18:08:07 +0200

    show todos with example fake

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,47 +1,46 @@
-// import React from "react";
+import React, { useState, useEffect } from "react";
 // import { showToDo } from "../api";
-// import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 
-// export default function Dashboard() {
-//   let [todoList, setTodoList] = useState({});
+export default function Dashboard() {
+  var todoList = [
+    { name: "this is test todo", status: "2", priority: "3", date: "23/05/19" }
+  ];
+  // let [todoList, setTodoList] = useState([]);
 
-//   componentDidMount () {
-//     event.preventDefault();
-//     showToDo().then(function (todoList){
-//       setTodoList(todoList)
-//     }
-//       )
-//     <
-//        //to do - multiple error msgs
+  // useEffect(() => {
+  //   showToDo().then(function(todos) {
+  //     setTodoList(todos);
+  //   });
+  // }, []);
 
-//   }
+  return (
+    <div>
+      <h1>DASHBOARD HERE</h1>
+      {todoList.map(function(todo) {
+        return (
+          <div>
+            <div> Description:{todo.name}</div>
+            <div> Status:{todo.status} </div>
+            <div> Priority:{todo.priority} </div>
+            <div> Date:{todo.date} </div>
+            <div>
+              <Button>Edit</Button>
+            </div>
+            <div>
+              <Button>Delete</Button>
+            </div>
+          </div>
+        );
+      })}
 
-//   return(
-//   <div>
-//   <h1>DASHBOARD HERE</h1>
-//     {this.todoList.map(function (todo){
-//       return(
-
-//         <div>
-//             <div> Description:{todo.text}</div>
-//             <div> Status:{todo.status} </div>
-//             <div> Priority:{todo.priority} </div>
-//             <div> Date:{todo.date} </div>
-//             <div><Button>Edit</Button></div>
-//             <div><Button>Delete</Button></div>
-//         </div>
-
-//       )
-
-//     })}
-
-//       <h1>ADD A TODO</h1>
-//       <form>
-//         <input></input>
-//       </form>
-
-//   </div>
-// }
+      <h1>ADD A TODO</h1>
+      <form>
+        <input />
+      </form>
+    </div>
+  );
+}
 //import login page
 //check for token localstorage.getItem("token")
 //redirect condition

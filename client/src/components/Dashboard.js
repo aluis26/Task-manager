978d2b749c14a16ebde34f85e23d05087c5e3be2
ALuis26 <annaluis26@gmail.com>
2019-08-13 13:30:07 +0200

    front end dashboard show all

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,8 +1,47 @@
-import React from "react";
+// import React from "react";
+// import { showToDo } from "../api";
+// import Button from 'react-bootstrap/Button';
 
-export default function Dashboard() {
-  return <h1>DASHBOARD HERE</h1>;
-}
+// export default function Dashboard() {
+//   let [todoList, setTodoList] = useState({});
+
+//   componentDidMount () {
+//     event.preventDefault();
+//     showToDo().then(function (todoList){
+//       setTodoList(todoList)
+//     }
+//       )
+//     <
+//        //to do - multiple error msgs
+
+//   }
+
+//   return(
+//   <div>
+//   <h1>DASHBOARD HERE</h1>
+//     {this.todoList.map(function (todo){
+//       return(
+
+//         <div>
+//             <div> Description:{todo.text}</div>
+//             <div> Status:{todo.status} </div>
+//             <div> Priority:{todo.priority} </div>
+//             <div> Date:{todo.date} </div>
+//             <div><Button>Edit</Button></div>
+//             <div><Button>Delete</Button></div>
+//         </div>
+
+//       )
+
+//     })}
+
+//       <h1>ADD A TODO</h1>
+//       <form>
+//         <input></input>
+//       </form>
+
+//   </div>
+// }
 //import login page
 //check for token localstorage.getItem("token")
 //redirect condition

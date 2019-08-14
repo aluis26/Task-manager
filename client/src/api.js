341c5d76c4120a5b2405950db5ae10021e80b743
Ainura Sagynova <ainura.sagyn@gmail.com>
2019-08-14 18:06:34 +0200

    update api part

--- client/src/api.js
+++ client/src/api.js
@@ -25,8 +25,7 @@ export function login(data) {
 export function signup(data) {
   console.log("signup data: ", data);
 
-  return (`/api/v1/signup`,
-  {
+  return fetch(`/api/v1/signup`, {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, cors, *same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached

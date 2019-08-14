2aa7b480ffbeae984105c85359b04f5ffceae875
Ainura Sagynova <ainura.sagyn@gmail.com>
2019-08-14 15:19:20 +0200

    basic styling added via bootsrap

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
 // import { showToDo } from "../api";
 import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 export default function Dashboard() {
   var todoList = [
@@ -16,28 +20,57 @@ export default function Dashboard() {
 
   return (
     <div>
-      <h1>DASHBOARD HERE</h1>
+      <h3>DASHBOARD HERE</h3>
       {todoList.map(function(todo) {
         return (
-          <div>
-            <div> Description:{todo.name}</div>
-            <div> Status:{todo.status} </div>
-            <div> Priority:{todo.priority} </div>
-            <div> Date:{todo.date} </div>
-            <div>
-              <Button>Edit</Button>
-            </div>
-            <div>
-              <Button>Delete</Button>
-            </div>
-          </div>
+          <Container>
+            <Row>
+              <Col xs>Description:{todo.name}</Col>
+              <Col xs>Status:{todo.status}</Col>
+              <Col xs>Priority:{todo.priority}</Col>
+              <Col xs>Date:{todo.date}</Col>
+              <Col xs>
+                <Button>Edit</Button>
+              </Col>
+              <Col xs>
+                <Button>Delete</Button>
+              </Col>
+            </Row>
+          </Container>
         );
       })}
 
-      <h1>ADD A TODO</h1>
-      <form>
-        <input />
-      </form>
+      <h3>ADD A TODO</h3>
+      <Container>
+        <Form>
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridTask">
+              <Form.Label>Todo task</Form.Label>
+              <Form.Control />
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPriority">
+              <Form.Label>Priority</Form.Label>
+              <Form.Control as="select">
+                <option>Choose...</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridDate">
+              <Form.Label>Date</Form.Label>
+              <Form.Control />
+            </Form.Group>
+          </Form.Row>
+
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }

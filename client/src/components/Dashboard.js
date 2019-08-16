dcd015bcd228f07bbb49b34fa34f53b3701d3107
unknown <alejandrapcabeza@gmail.com>
2019-08-16 16:49:57 +0200

    conflict fixed and basic styling

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,11 +6,154 @@ import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Modal from "react-bootstrap/Modal";
-import React from "react";
-import TodoList from "./TodoList";
 import Quotes from "./Quotes";
 
 export default function Dashboard() {
+  let [task, setTask] = useState("");
+
+  let [priority, setPriority] = useState("");
+  let [dueDate, setDueDate] = useState("");
+  let [modalShow, setModalShow] = useState(false);
+  let [todoList, setTodoList] = useState([]);
+  let [todoId, setTodoId] = useState("");
+
+  function handleAddTask(event) {
+    setTask(event.target.value);
+    console.log("Task-", task);
+  }
+
+  function handleAddPriority(event) {
+    var prio = event.target.value;
+    prio = parseInt(prio);
+    setPriority(prio);
+    console.log("Priority-", event.target.value);
+  }
+
+  function handleAddDate(event) {
+    setDueDate(event.target.value);
+    console.log("Date-", event.target.value);
+  }
+
+  function handleDelete(id) {
+    console.log(id);
+    deleteToDo(id).then(response => {
+      console.log("message", response);
+      showToDo().then(function(response) {
+        console.log("message", response);
+        setTodoList(response.data.result);
+      });
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let data = { task, priority, dueDate, status: 0 };
+    console.log(data);
+    if (task) {
+      addToDo(data).then(response => {
+        console.log(response.message);
+        showToDo().then(function(response) {
+          console.log(response);
+          setTodoList(response.data.result);
+        });
+      });
+    }
+  }
+
+  useEffect(() => {
+    showToDo().then(function(response) {
+      setTodoList(response.data.result);
+    });
+  }, []);
+  console.log(todoList);
+
+  function MydModalWithGrid(props) {
+    var selectedTodo = todoList.filter(todo => todo.id == todoId);
+    let [editTask, setEditTask] = useState("");
+
+    function handleEditTask(event) {
+      setEditTask(event.target.value);
+      console.log("editTask-", event.target.value);
+    }
+
+    function handleSubmitEdit(event) {
+      event.preventDefault();
+      let data = { task: editTask, id: todoId };
+      console.log("data", data);
+      if (data) {
+        editToDo(data).then(response => {
+          console.log(response.message);
+          showToDo().then(function(response) {
+            console.log(response);
+            setTodoList(response.data.result);
+          });
+        });
+      }
+    }
+
+    if (selectedTodo.length === 1) {
+      return (
+        <Modal
+          size="xl"
+          {...props}
+          aria-labelledby="contained-modal-title-vcenter"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Edit your task:
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Container>
+              <Row className="show-grid">
+                <Col xs={6} md={6}>
+                  <Form.Group as={Col} controlId="formGridTask">
+                    <Form.Label>Todo task</Form.Label>
+                    <Form.Control
+                      value={editTask}
+                      placeholder={selectedTodo[0].task}
+                      onChange={event => handleEditTask(event)}
+                    />
+                  </Form.Group>
+                </Col>
+                <Col xs={2} md={2}>
+                  <Form.Group as={Col} md="2" controlId="formGridTask">
+                    <Form.Label>Status</Form.Label>
+                    <Form.Control />
+                  </Form.Group>
+                </Col>
+                <Col xs={2} md={2}>
+                  <Form.Group as={Col} md="2" controlId="formGridTask">
+                    <Form.Label>Priority</Form.Label>
+                    <Form.Control />
+                  </Form.Group>
+                </Col>
+                <Col xs={2} md={2}>
+                  <Form.Group as={Col} md="2" controlId="formGridDate">
+                    <Form.Label>Date</Form.Label>
+                    <Form.Control
+                      type="date"
+                      value={dueDate}
+                      onChange={event => handleAddDate(event)}
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+            </Container>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button type="submit" onClick={handleSubmitEdit}>
+              Submit
+            </Button>
+            <Button onClick={props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      );
+    } else {
+      return null;
+    }
+  }
+
   return (
     <div>
       <Container>
@@ -112,10 +255,10 @@ export default function Dashboard() {
           </Form.Row>
         </Form>
       </Container>
-    </div>
-    <React.Fragment>
-      <TodoList />
       <Quotes />
-    </React.Fragment>
+    </div>
   );
 }
+//import login page
+//check for token localstorage.getItem("token")
+//redirect condition

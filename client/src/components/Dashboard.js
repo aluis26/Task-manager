04b4e9d1f0828dae3f5b43b4ef34b14a8c5eee15
unknown <alejandrapcabeza@gmail.com>
2019-08-17 13:30:41 +0200

    sign up redirection fixed

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,12 @@
 import React from "react";
 import TodoList from "./TodoList";
+import Quotes from "./Quotes";
 
 export default function Dashboard() {
   return (
     <React.Fragment>
       <TodoList />
+      <Quotes />
     </React.Fragment>
   );
 }
